63a2dd585cbaab488a1985555bf88fe2bff44880
wangbingzhi <853244870@qq.com>
2018-05-21 20:46:19 +0800

    111

---
+++ src/com/afterclasswork/Demo.java
@@ -0,0 +1,35 @@
+package com.afterclasswork;
+
+import com.stack.LinkStack;
+import com.stack.SqStack;
+
+public class Demo {
+
+    //编写一个函数，要求借助一个栈把一个数组中的元素逆置
+    public void reverse(Object[] a) throws Exception{
+        SqStack sqStack = new SqStack(a.length);
+        for (int i = 0; i <a.length ; i++) {
+            sqStack.push(a[i]);
+        }
+        for (int i = 0; i <a.length ; i++) {
+            a[i] = sqStack.pop();
+        }
+    }
+
+    //编写一个函数判断一个字符序列是否是回文序列，例如abba和abdba都属于回文序列，要求只使用栈来实现
+    //判断字符序列是否是回文序列，是返回true,不是返回false
+    public boolean isPalindSeq(String str) throws Exception {
+        LinkStack linkStack = new LinkStack();
+        int i = 0;
+        for (; i <str.length() ; i++) {
+            linkStack.push(str.charAt(i));
+        }
+        for (i = 0; i <str.length() ; i++) {
+            char c = ((Character)linkStack.pop()).charValue();
+            if(c!=str.charAt(i)){
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ src/com/util/Test.java
@@ -0,0 +1,71 @@
+package com.util;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.threenumscompare3();
+    }
+    public void maopaosort() {
+        int arr[] = {6, 5, 9, 3, 1, 46};
+        System.out.println("排序前的数组为：");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        for (int i = 0; i < arr.length - 1; i++) { //外层循环控制排序执行几趟
+            for (int j = 0; j < arr.length - 1 - i; j++) { //内层循环控制每趟排序多少次
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("------------------------------------------");
+        System.out.println("排序后的数组为：");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+        public void twonumscompare(){
+            Scanner scanner = new Scanner(System.in);
+            int a,b,max;
+            System.out.println("请输入a:");
+            a = scanner.nextInt();
+            System.out.println("请输入b:");
+            b = scanner.nextInt();
+            max = a>b?a:b;
+            System.out.println("最大值是："+max);
+
+    }
+    public void threenumscompare(){
+        Scanner scanner = new Scanner(System.in);
+        int a,b,c,d;
+
+        System.out.println("请输入a:");
+        a = scanner.nextInt();
+        System.out.println("请输入b:");
+        b = scanner.nextInt();
+        System.out.println("请输入c:");
+        c = scanner.nextInt();
+
+        d = a>b?a:b;
+        int max = d>c?d:c;
+
+        System.out.println(max);
+    }
+
+    public void sortcompare3(){
+     int[] array = {2,1,4};
+        Arrays.sort(array);
+        /*
+        for (int i = 0; i <array.length ; i++) {
+            System.out.println(array[i]);
+        }*/
+        for (int num: array) {
+            System.out.println(num);
+        }
+    }
+}
